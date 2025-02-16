3167262ccdaf8709a6da88f62d942ea1fa2631a6
Sergey Gorshkov <sergejgorshkov@yandex.ru>
2025-02-16 21:38:47 +0500

    В widget.py добавлен код для тестирования: import os, пути к файлам в папке test, блоки для тестирования функций

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,12 @@
+import os  #  Для тестирования функций
 from datetime import datetime
 
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
+
+# Константы путей к файлам для тестирования функций mask_account_card() и get_date()
+PATH_TO_TEST_MASK = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tests", "for_testing_masks.txt")
+PATH_TO_TEST_DATE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tests", "for_testing_date.txt")
 
 
 def mask_account_card(data_for_mask: str) -> str:
@@ -31,3 +36,17 @@ def get_date(date_for_change: str) -> str:
     formatted_date = date_datetime.strftime("%d.%m.%Y")
 
     return formatted_date
+
+# Блок для тестирования функции mask_account_card()
+with open(PATH_TO_TEST_MASK, "r", encoding="utf-8") as file:
+    numbers_list = file.readlines()  # чтение файла и построчное разделение на элементы списка
+    cleaned_number_list = [number.strip() for number in numbers_list]
+    for number in cleaned_number_list:
+        print(mask_account_card(number))
+
+# Блок для тестирования функции get_date()
+with open(PATH_TO_TEST_DATE, "r", encoding="utf-8") as file:
+    numbers_list = file.readlines()  # чтение файла и построчное разделение на элементы списка
+    cleaned_number_list = [number.strip() for number in numbers_list]
+    for number in cleaned_number_list:
+        print(get_date(number))
