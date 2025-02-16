82879c402fd5257a58bc57834a00191bd3e6eb96
Sergey Gorshkov <sergejgorshkov@yandex.ru>
2025-02-16 21:32:32 +0500

    В widget.py добавлена функция mask_account_card(), добавлены импорты функций из модуля masks.py

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,33 @@
+from datetime import datetime
+
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
 def mask_account_card(data_for_mask: str) -> str:
-    """Функция маскировки номера карты или счета"""
+    """
+    Функция маскировки номера карты или счета. Принимает строку с названием и номером карты или счета.
+    Возвращает название карты или счета с маской.
+    """
+    data_list = data_for_mask.split(" ")
+    for element in data_list:
+        if element.isdigit():
+            if len(element) == 16:
+                result = data_for_mask.replace(element, get_mask_card_number(element))
+                break
+            elif len(element) == 20:
+                result = data_for_mask.replace(element, get_mask_account(element))
+                break
+        result = f"Ошибка! В данных '{data_for_mask}' номер карты или счета не обнаружен."
 
-    pass
-
+    return result
 
 
 def get_date(date_for_change: str) -> str:
-    """Функция приведения даты к виду 'ДД.ММ.ГГГГ'"""
-    from datetime import datetime
+    """
+    Функция форматирования даты. Принимает строку с датой в формате ISO 8601 и приводит ее к виду "ДД.ММ.ГГГГ".
+    """
     date_datetime = datetime.fromisoformat(date_for_change)
     formatted_date = date_datetime.strftime("%d.%m.%Y")
 
     return formatted_date
-
-# print(get_date("2024-03-11T02:26:18.671407"))
