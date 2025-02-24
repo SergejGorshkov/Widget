2a863315b2163e254b911bf78ec216446c39187e
SergejGorshkov <gorshkovserg@gmail.com>
2025-02-24 19:33:59 +0500

    Исправлены ошибки аннотации типов в processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,9 @@
-def filter_by_state(data_of_bank_operations: list[dict[str, [str | int]]], key_state: str="EXECUTED") \
-        -> list[dict[str, [str | int]]]:
+from typing import Dict, List, Union
+
+
+def filter_by_state(
+    data_of_bank_operations: List[Dict[str, Union[str, int]]], key_state: str = "EXECUTED"
+) -> List[Dict[str, Union[str, int]]]:
     """Функция принимает список словарей и, опционально, значение для ключа "state" (по умолчанию "EXECUTED").
     Возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному
     значению."""
@@ -8,16 +12,22 @@ def filter_by_state(data_of_bank_operations: list[dict[str, [str | int]]], key_s
     return filtered_list
 
 
-def sort_by_date(data_of_bank_operations: list[dict[str, [str | int]]], sort_key: str="True"):
+def sort_by_date(
+    data_of_bank_operations: List[Dict[str, Union[str, int]]], sort_key: Union[str, bool] = "True"
+) -> List[Dict[str, Union[str, int]]]:
     """Функция принимает список словарей и необязательный параметр (sort_key) в виде строки,
     задающий порядок сортировки (по умолчанию — убывание).
     Возвращает новый список, отсортированный по дате."""
 
-    if sort_key.lower() == "true":
-        sort_key = True
-    elif sort_key.lower() == "false":
-        sort_key = False
-    # Блок проверки значения ключа для направления сортировки данных по дате
+    if isinstance(sort_key, str):
+        # Если ключ для определения направления сортировки - строка, выполняем анализ его значения
+        if sort_key.lower() == "true":
+            sort_key = True
+        elif sort_key.lower() == "false":
+            sort_key = False
+        else:
+            raise ValueError("Invalid value for sort_key")
+        # Блок проверки значения ключа для направления сортировки данных по дате
 
-    sorted_data = sorted(data_of_bank_operations, key=lambda operation: operation.get("date"), reverse=sort_key)
+    sorted_data = sorted(data_of_bank_operations, key=lambda operation: operation.get("date", ""), reverse=sort_key)
     return sorted_data
