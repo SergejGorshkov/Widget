d4059fa5c92222f30f73588081d1a63672e8f0a9
SergejGorshkov <gorshkovserg@gmail.com>
2025-03-29 15:32:37 +0500

    Добавлено логирование для модулей masks.py, utils.py. Готово к сдаче

--- src/masks.py
+++ src/masks.py
@@ -3,13 +3,23 @@
 (например: 3232323245456565) и номера счета (20 цифр) (например: 90903209302935646897) и
 формирования их масок в формате '3232 32** **** 6565' и '**6897' соответственно.
 """
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_mask_card_number(number: str) -> str:
     """Функция маскировки номера банковской карты"""
+    logger.debug("Создана маска банковской карты.")
     return f"{number[0:4]} {number[4:6]}** **** {number[-4:]}"
 
 
 def get_mask_account(number: str) -> str:
     """Функция маскировки номера банковского счета"""
+    logger.debug("Создана маска банковского счета.")
     return f"**{number[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,17 @@
 import json
-
+import logging
 
 # import os
 # Path_to_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
 # Путь для тестирования работы функции
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_json_file(path_to_file: str) -> list[dict]:
@@ -12,17 +20,24 @@ def read_json_file(path_to_file: str) -> list[dict]:
     Если файл пустой, содержит не-список или не найден, функция возвращает пустой список."""
     data_json = []  # По умолчанию данные отсутствуют
     try:
+        logger.info("Чтение данных из JSON-файла...")
         with open(path_to_file, "r", encoding="UTF-8") as file:
             data_json = json.load(file)  # Если данные получены без ошибок, значение 'data_json' перезаписывается
+            logger.info("Данные из JSON-файла успешно получены.")
     except json.JSONDecodeError:
         print("Ошибка декодирования файла.")
+        logger.error("Произошла ошибка декодирования файла.")
     except FileNotFoundError:
         print(f"Ошибка! Файл по адресу {path_to_file} не найден.")
+        logger.error(f"Ошибка! Файл по адресу {path_to_file} не найден.")
+
     if not isinstance(data_json, list):
+        logger.warning("Данные из JSON-файла неправильного формата.")
         return []
 
     return data_json  # Возврат содержимого JSON-файла или пустого списка в случае, если файл пустой, содержит
     # не-список или не найден
+
 
 #################################################################################################################
 
